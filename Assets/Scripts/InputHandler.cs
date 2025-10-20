fc53ed96798b278f0ebff17c6878391d5672858f
DavudNG <brokenarma@gmail.com>
2025-10-20 14:48:38 +1100

    Ver 2.0 3D level update
    Added:
    - 3d level prototype level4 scene
    - 3d level assets
    - inputhandler.cs
    - playermovementn3d.cs

---
+++ Assets/Scripts/InputHandler.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputHandler : MonoBehaviour
+{
+    public PlayerMovement3D myMovementScript;
+
+    private InputAction movementAction , movementaction2;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
