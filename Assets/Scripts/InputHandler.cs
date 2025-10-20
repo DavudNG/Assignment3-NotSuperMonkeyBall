b101e0fb9694eeb583ed2dfc6726b78d7affed67
DavudNG <brokenarma@gmail.com>
2025-10-21 00:10:46 +1100

    Ver 2.1 3D Movement
    Changed:
    
    - Functionality added to Playermovement3d.cs and Inputhander
    
    - added movement to the player in level 4

--- Assets/Scripts/InputHandler.cs
+++ Assets/Scripts/InputHandler.cs
@@ -5,17 +5,26 @@ public class InputHandler : MonoBehaviour
 {
     public PlayerMovement3D myMovementScript;
 
-    private InputAction movementAction , movementaction2;
+    private InputAction _movementAction , _jumpAction, _attackAction, _upperAction;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        _movementAction = InputSystem.actions.FindAction("Move");
+        _jumpAction = InputSystem.actions.FindAction("Jump");
+        _attackAction = InputSystem.actions.FindAction("Attack");
+        _upperAction = InputSystem.actions.FindAction("Uppercut");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 movementVector = _movementAction.ReadValue<Vector2>();
+        myMovementScript.Move(movementVector);
+
+        if(_jumpAction.triggered)
+        {
+            myMovementScript.Jump();
+        }
     }
 }
--- Assets/Scripts/PlayerMovement3D.cs
+++ Assets/Scripts/PlayerMovement3D.cs
@@ -2,6 +2,21 @@ using UnityEngine;
 
 public class PlayerMovement3D : MonoBehaviour
 {
+    public CharacterController myCharacterController;
+    public float movementSpeedMultiplier;
+    public float jumpForce;
+    public float jumpForceMultiplier;
+    public float gravityForce;
+    public float gravityForceMultiplier;
+    
+    public Vector3 raySize; // size of the ray cast to use
+    public float castDistance; // off set of the ray cast to use
+    public LayerMask groundLayer; // ref to the groundlayer tag
+    public LayerMask interactableLayer; // ref to the interactable layer tag
+
+    //public bool debugGrounded;
+    //public LayerMask debugInteractable;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +26,54 @@ public class PlayerMovement3D : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //debugGrounded = isGrounded();
+        //Debug.DrawRay(transform.position, Vector3.down, Color.black);
+    }
+
+    private void FixedUpdate()
+    {
+        if (!isGrounded())
+        {
+            myCharacterController.Move(new Vector3(0, -gravityForce * gravityForceMultiplier * Time.deltaTime, 0));
+        }
+    }
+
+    public void Move(Vector2 movementVector)
+    {
+        Vector3 v3ToMove = new Vector3 (movementVector.x, 0 , movementVector.y);
+        v3ToMove = v3ToMove * movementSpeedMultiplier * Time.deltaTime;
+        myCharacterController.Move(v3ToMove);
+    }
+
+    public void Jump()
+    {
+        if(isGrounded())
+            myCharacterController.Move(new Vector3(0, jumpForce * jumpForceMultiplier * Time.deltaTime, 0));
+    }
+
+    public bool isGrounded()
+    {
+            //RaycastHit hit;
+            // checks whether the raycast returns true if it collides with either the groundlayer or interactable
+            if (Physics.Raycast(transform.position, Vector3.down, castDistance, groundLayer) || Physics.Raycast(transform.position, Vector3.down, castDistance, interactableLayer))
+            {
+                //Physics.BoxCast(transform.position, raySize, Vector3.down, out hit, Quaternion.identity, castDistance, groundLayer)
+                //debugInteractable = hit.collider.gameObject.layer;
+                
+                // if true
+                return true;
+            }
+
+            else
+            {
+                // if nothing
+                return false;
+            }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Vector3 distance = Vector3.down * castDistance;
+        Gizmos.DrawRay(transform.position, distance);
     }
 }
