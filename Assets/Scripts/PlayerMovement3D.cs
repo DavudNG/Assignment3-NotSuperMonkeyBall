5efb32096fd330a956dd32da632c26ef86e06a9a
DavudNG <brokenarma@gmail.com>
2025-10-21 14:34:06 +1100

    Ver 2.2 Animation Update
    -Added player animations for 3d level
     - Attacking, moving, landing, jump
     - combined animations for moving/jumping and atking via upper body avatar mask

--- Assets/Scripts/PlayerMovement3D.cs
+++ Assets/Scripts/PlayerMovement3D.cs
@@ -3,12 +3,17 @@ using UnityEngine;
 public class PlayerMovement3D : MonoBehaviour
 {
     public CharacterController myCharacterController;
+    public Animator myAnimator; // Ref to the Animator of the player
+
     public float movementSpeedMultiplier;
     public float jumpForce;
     public float jumpForceMultiplier;
     public float gravityForce;
     public float gravityForceMultiplier;
-    
+    public float rotationSpeed;
+    public float minimumJumpTime;
+    public float currentJumpTime;
+
     public Vector3 raySize; // size of the ray cast to use
     public float castDistance; // off set of the ray cast to use
     public LayerMask groundLayer; // ref to the groundlayer tag
@@ -28,11 +33,39 @@ public class PlayerMovement3D : MonoBehaviour
     {
         //debugGrounded = isGrounded();
         //Debug.DrawRay(transform.position, Vector3.down, Color.black);
+       //myAnimator.SetFloat("speed", Mathf.Abs(movementInput.x)); // sets the paramater in animation statemachine for the animation to play move
+       //myAnimator.SetFloat("vertical_speed", rb.linearVelocity.y); // sets the paramater in animation statemachine for the animation to play jump
+        myAnimator.SetBool("isGrounded", isGrounded()); // sets the paramater in animation statemachine for the animation to return to idle
+        
+        if(myCharacterController.velocity.x > 0 || myCharacterController.velocity.x < 0 || myCharacterController.velocity.z > 0 || myCharacterController.velocity.z < 0)
+        {
+            myAnimator.SetBool("isMoving", true);
+        }
+        else
+        {
+            myAnimator.SetBool("isMoving", false);
+        }
+        
+        if (currentJumpTime > 0)
+        {
+            currentJumpTime -= Time.deltaTime;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            Attack();
+        }
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Uppercut();
+            Debug.Log("Test");
+        }
     }
 
     private void FixedUpdate()
     {
-        if (!isGrounded())
+        if (!isGrounded() && currentJumpTime <= 0)
         {
             myCharacterController.Move(new Vector3(0, -gravityForce * gravityForceMultiplier * Time.deltaTime, 0));
         }
@@ -41,6 +74,12 @@ public class PlayerMovement3D : MonoBehaviour
     public void Move(Vector2 movementVector)
     {
         Vector3 v3ToMove = new Vector3 (movementVector.x, 0 , movementVector.y);
+        if(v3ToMove != Vector3.zero)
+        {
+            Quaternion rotation = Quaternion.LookRotation(v3ToMove);
+            //transform.rotation = rotation;
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, rotation, rotationSpeed * Time.deltaTime);
+        }
         v3ToMove = v3ToMove * movementSpeedMultiplier * Time.deltaTime;
         myCharacterController.Move(v3ToMove);
     }
@@ -48,7 +87,21 @@ public class PlayerMovement3D : MonoBehaviour
     public void Jump()
     {
         if(isGrounded())
+        {
             myCharacterController.Move(new Vector3(0, jumpForce * jumpForceMultiplier * Time.deltaTime, 0));
+            currentJumpTime = minimumJumpTime;
+        }
+            
+    }
+
+    public void Attack()
+    {
+        myAnimator.SetTrigger("isAttacking");
+    }
+
+    public void Uppercut()
+    {
+        myAnimator.SetTrigger("isUppercutting");
     }
 
     public bool isGrounded()
