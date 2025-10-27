a6b938b078ff8c445ad53ec3109de78d68aa0963
AngusDoddrell1 <104553074@student.swin.edu.au>
2025-10-27 17:47:48 +1100

    Dying and Pausing in 3D level

---
+++ Assets/Scripts/3D/BallHealth3D.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class BallHealth3D : MonoBehaviour
+{
+    public int health = 3;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Handle logic when the ball falls off the platform
+        if(this.transform.position.y < 45)
+        {
+            // Remove 100 health killing the ball
+            registerHit(100);
+        }
+    }
+
+    // Use a function inside the ball to register a hit
+    // This can be used outside of this script providing basically a "setter" for the balls health
+    // Use a default value of 1, however this can be overridden
+    void registerHit(int damage = 1)
+    {
+        // Subtract one from health
+        health -= damage;
+        // This triggers when the ball dies
+        if (health <= 0)
+        {
+            DeathMenuScript deathMenu = FindObjectOfType<DeathMenuScript>();
+            deathMenu.DisplayDeathScreen();
+        }
+    }
+}
