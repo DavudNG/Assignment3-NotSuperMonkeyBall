2275cc3d297d0aaf355ee5613126dc6b555ebee3
jamnewb <104563879@student.swin.edu.au>
2025-10-29 04:00:11 +1100

    Fixed Difficulty Data and Gave Enemies Health
    I fixed the difficulty data and gave enemies

--- Assets/Scripts/DifficultyData.cs
+++ Assets/Scripts/DifficultyData.cs
@@ -4,8 +4,6 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "DifficultyData", menuName = "Custom/DifficultyData")]
 public class DifficultyData : ScriptableObject
 {
-    public float jumpForce = 20;
-    public float moveSpeed = 35;
-
-    // add enemy values
+    public float speed;
+    public int health;
 }
